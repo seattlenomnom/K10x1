25539b7797e9ae140c0791d1a3046fee25f1d443
seattlenomnom <mark.crapser@gmail.com>
2023-08-26 22:00:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# K10x1
+A text mule to practice elementary concepts that will be used to solve problem
+7 of chapter 10 of, "Kochan, Stephen, Programming in C 4th ed."
+## Things to do
+## Things done
---
+++ k10x1.c
@@ -0,0 +1,39 @@
+/* source file for k10x1
+ *
+*/
+
+/* includes */
+#include <stdio.h>
+#include <stddef.h>
+
+
+/* defines */
+
+
+/* structure defines */
+
+
+/* function declarations */
+
+
+/* main */
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+
+
+    return(0);
+}
+
+/* function definitions */
+
+
+
+
+
+
+
